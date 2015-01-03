3c525db27efba5a8d96e060725dc223428167580
Kevin Sylvestre <kevin@ksylvest.com>
2015-01-02 18:07:57 -0800

    added in a CLI that can be tested

--- lib/enigma/cli.rb
+++ lib/enigma/cli.rb
@@ -3,13 +3,47 @@ require 'slop'
 module Enigma
   class CLI
 
-    def initialize
+    attr_accessor :machine
+    attr_accessor :input
+    attr_accessor :output
+
+    def initialize(input: STDIN, output: STDOUT)
+      self.machine = Machine.new
+      self.input  = input
+      self.output = output
+    end
+
+    def parse
       Slop.parse do |options|
-        options.on '-v', '--version', 'version' do
-          puts ::Enigma::VERSION
-          exit
+        options.banner = "usage: enigma [options] ..."
+
+        options.on '-h', '--help' do
+          help(options)
+          return
         end
+
+        options.on '-v', '--version' do
+          version(options)
+          return
+        end
+
       end
+
+      convert
+    end
+
+  private
+
+    def help(options = {})
+      self.output.puts(options)
+    end
+
+    def version(options = {})
+      self.output.puts(::Enigma::VERSION)
+    end
+
+    def convert
+      self.output.puts(self.machine.convert(self.input.gets.chomp))
     end
 
   end

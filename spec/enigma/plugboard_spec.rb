d95433d9b30559eff1f795b10f804592993b2d63
Kevin Sylvestre <kevin@ksylvest.com>
2015-01-02 19:45:06 -0800

    some simple specs

--- spec/enigma/plugboard_spec.rb
+++ spec/enigma/plugboard_spec.rb
@@ -4,4 +4,12 @@ describe Enigma::Plugboard do
 
   let (:plugboard) { Enigma::Plugboard.new }
 
+  describe "#swap" do
+    it "swaps a character after plugging" do
+      plugboard.plug('A','B')
+      expect(plugboard.swap('A')).to eql('B')
+      expect(plugboard.swap('B')).to eql('A')
+    end
+  end
+
 end

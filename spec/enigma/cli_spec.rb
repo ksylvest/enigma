08c9c05886bfe27458a6479a77d9c92847a28760
Kevin Sylvestre <kevin@ksylvest.com>
2015-01-04 13:32:20 -0800

    fixing coverage

--- spec/enigma/cli_spec.rb
+++ spec/enigma/cli_spec.rb
@@ -6,6 +6,17 @@ describe Enigma::CLI do
   let (:output) { double('output') }
   let (:cli) { Enigma::CLI.new(input: input, output: output) }
 
+  describe "#parse" do
+
+    it "stubs to slop" do
+      expect(Slop).to receive(:parse)
+      expect(input).to receive(:gets) { "Turing" }
+      expect(output).to receive(:puts)
+      cli.parse()
+    end
+
+  end
+
   describe "#execute" do
 
     it "converts input to output using the streams" do

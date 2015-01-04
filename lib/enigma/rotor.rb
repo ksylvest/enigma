a3d18bcfba0a54a175333a50050df33a46798ef6
Kevin Sylvestre <kevin@ksylvest.com>
2015-01-04 14:00:45 -0800

    freezing

--- lib/enigma/rotor.rb
+++ lib/enigma/rotor.rb
@@ -15,11 +15,11 @@ module Enigma
 
     module Mappings
       DEFAULT = ALPHABET
-      A = %w(E K M F L G D Q V Z N T O W Y H X U S P A I B R C J)
-      B = %w(A J D K S I R U X B L H W T M C Q G Z N P Y F V O E)
-      C = %w(B D F H J L C P R T X V Z N Y E I W G A K M U S Q O)
-      D = %w(E S O V P Z J A Y Q U I R H X L N F T G K D C M W B)
-      E = %w(V Z B R G I T Y U P S D N H L X A W M J Q O F E C K)
+      A = %w(E K M F L G D Q V Z N T O W Y H X U S P A I B R C J).freeze
+      B = %w(A J D K S I R U X B L H W T M C Q G Z N P Y F V O E).freeze
+      C = %w(B D F H J L C P R T X V Z N Y E I W G A K M U S Q O).freeze
+      D = %w(E S O V P Z J A Y Q U I R H X L N F T G K D C M W B).freeze
+      E = %w(V Z B R G I T Y U P S D N H L X A W M J Q O F E C K).freeze
     end
 
     module Turnover
